2589064ca85c32f1373798cfe60a3eac792d409c
f171a9a3497c8b <f171a9a3497c8b@users.noreply.github.com>
2019-10-21 15:50:33 +0200

    Add solution to Rosetta Code problem - IBAN

--- src/RosettaCode_solutions.js
+++ src/RosettaCode_solutions.js
@@ -79,6 +79,9 @@
  * I before E except after C
  * https://learn.freecodecamp.org/coding-interview-prep/rosetta-code/i-before-e-except-after-c/
  *
+ * IBAN
+ * https://www.freecodecamp.org/learn/coding-interview-prep/rosetta-code/iban
+ *
  * Identity matrix
  * https://www.freecodecamp.org/learn/coding-interview-prep/rosetta-code/identity-matrix
  *
@@ -811,6 +814,163 @@ function IBeforeExceptC(word) {
     const breakedRule2 = (/[^c]ei/).test(word); // "E before I when preceded by C"
 
     return !(breakedRule1 || breakedRule2);
+}
+
+
+// IBAN
+const IBAN_FORMATS = {
+    AD: /^AD\d{2}\d{8}[a-zA-Z0-9]{12}$/, // Andorra
+    AE: /^AE\d{2}\d{19}$/, // United Arab Emirates
+    AL: /^AL\d{2}\d{8}[a-zA-Z0-9]{16}$/, // Albania
+    AT: /^AT\d{2}\d{16}$/, // Austria
+    AZ: /^AZ\d{2}[A-Z]{4}[a-zA-Z0-9]{20}$/, // Azerbaijan
+    BA: /^BA\d{2}\d{16}$/, // Bosnia and Herzegovina
+    BE: /^BE\d{2}\d{12}$/, // Belgium
+    BG: /^BG\d{2}[A-Z]{4}\d{6}[a-zA-Z0-9]{8}$/, // Bulgaria
+    BH: /^BH\d{2}[A-Z]{4}[a-zA-Z0-9]{14}$/, // Bahrain
+    BR: /^BR\d{2}\d{23}[A-Z]{1}[a-zA-Z0-9]{1}$/, // Brazil
+    BY: /^BY\d{2}[a-zA-Z0-9]{4}\d{4}[a-zA-Z0-9]{16}$/, // Republic of Belarus
+    CH: /^CH\d{2}\d{5}[a-zA-Z0-9]{12}$/, // Switzerland
+    CR: /^CR\d{2}\d{18}$/, // Costa Rica
+    CY: /^CY\d{2}\d{8}[a-zA-Z0-9]{16}$/, // Cyprus
+    CZ: /^CZ\d{2}\d{20}$/, // Czech Republic
+    DE: /^DE\d{2}\d{18}$/, // Germany
+    DK: /^DK\d{2}\d{14}$/, // Denmark
+    DO: /^DO\d{2}[a-zA-Z0-9]{4}\d{20}$/, // Dominican Republic
+    EE: /^EE\d{2}\d{16}$/, // Estonia
+    ES: /^ES\d{2}\d{20}$/, // Spain
+    FI: /^FI\d{2}\d{14}$/, // Finland
+    FO: /^FO\d{2}\d{14}$/, // Faroe Islands
+    FR: /^FR\d{2}\d{10}[a-zA-Z0-9]{11}\d{2}$/, // France
+    GB: /^GB\d{2}[A-Z]{4}\d{14}$/, // United Kingdom
+    GE: /^GE\d{2}[A-Z]{2}\d{16}$/, // Georgia
+    GI: /^GI\d{2}[A-Z]{4}[a-zA-Z0-9]{15}$/, // Gibraltar
+    GL: /^GL\d{2}\d{14}$/, // Greenland
+    GR: /^GR\d{2}\d{7}[a-zA-Z0-9]{16}$/, // Greece
+    GT: /^GT\d{2}[a-zA-Z0-9]{24}$/, // Guatemala
+    HR: /^HR\d{2}\d{17}$/, // Croatia
+    HU: /^HU\d{2}\d{24}$/, // Hungary
+    IE: /^IE\d{2}[A-Z]{4}\d{14}$/, // Ireland
+    IL: /^IL\d{2}\d{19}$/, // Israel
+    IQ: /^IQ\d{2}[A-Z]{4}\d{15}$/, // Iraq
+    IS: /^IS\d{2}\d{22}$/, // Iceland
+    IT: /^IT\d{2}[A-Z]{1}\d{10}[a-zA-Z0-9]{12}$/, // Italy
+    JO: /^JO\d{2}[A-Z]{4}\d{4}[a-zA-Z0-9]{18}$/, // Jordan
+    KW: /^KW\d{2}[A-Z]{4}[a-zA-Z0-9]{22}$/, // Kuwait
+    KZ: /^KZ\d{2}\d{3}[a-zA-Z0-9]{13}$/, // Kazakhstan
+    LB: /^LB\d{2}\d{4}[a-zA-Z0-9]{20}$/, // Lebanon
+    LC: /^LC\d{2}[A-Z]{4}[a-zA-Z0-9]{24}$/, // Saint Lucia
+    LI: /^LI\d{2}\d{5}[a-zA-Z0-9]{12}$/, // Liechtenstein
+    LT: /^LT\d{2}\d{16}$/, // Lithuania
+    LU: /^LU\d{2}\d{3}[a-zA-Z0-9]{13}$/, // Luxembourg
+    LV: /^LV\d{2}[A-Z]{4}[a-zA-Z0-9]{13}$/, // Latvia
+    MC: /^MC\d{2}\d{10}[a-zA-Z0-9]{11}\d{2}$/, // Monaco
+    MD: /^MD\d{2}[a-zA-Z0-9]{20}$/, // Moldova
+    ME: /^ME\d{2}\d{18}$/, // Montenegro
+    MK: /^MK\d{2}\d{3}[a-zA-Z0-9]{10}\d{2}$/, // Macedonia
+    MR: /^MR\d{2}\d{23}$/, // Mauritania
+    MT: /^MT\d{2}[A-Z]{4}\d{5}[a-zA-Z0-9]{18}$/, // Malta
+    MU: /^MU\d{2}[A-Z]{4}\d{19}[A-Z]{3}$/, // Mauritius
+    NL: /^NL\d{2}[A-Z]{4}\d{10}$/, // Netherlands
+    NO: /^NO\d{2}\d{11}$/, // Norway
+    PK: /^PK\d{2}[A-Z]{4}[a-zA-Z0-9]{16}$/, // Pakistan
+    PL: /^PL\d{2}\d{24}$/, // Poland
+    PS: /^PS\d{2}[A-Z]{4}[a-zA-Z0-9]{21}$/, // Palestine
+    PT: /^PT\d{2}\d{21}$/, // Portugal
+    QA: /^QA\d{2}[A-Z]{4}[a-zA-Z0-9]{21}$/, // Qatar
+    RO: /^RO\d{2}[A-Z]{4}[a-zA-Z0-9]{16}$/, // Romania
+    RS: /^RS\d{2}\d{18}$/, // Serbia
+    SA: /^SA\d{2}\d{2}[a-zA-Z0-9]{18}$/, // Saudi Arabia
+    SC: /^SC\d{2}[A-Z]{4}\d{20}[A-Z]{3}$/, // Seychelles
+    SE: /^SE\d{2}\d{20}$/, // Sweden
+    SI: /^SI\d{2}\d{15}$/, // Slovenia
+    SK: /^SK\d{2}\d{20}$/, // Slovakia
+    SM: /^SM\d{2}[A-Z]{1}\d{10}[a-zA-Z0-9]{12}$/, // San Marino
+    ST: /^ST\d{2}\d{21}$/, // Sao Tome and Principe
+    SV: /^SV\d{2}[A-Z]{4}\d{20}$/, // El Salvador
+    TL: /^TL\d{2}\d{19}$/, // Timor-Leste
+    TN: /^TN\d{2}\d{20}$/, // Tunisia
+    TR: /^TR\d{2}\d{6}[a-zA-Z0-9]{16}$/, // Turkey
+    UA: /^UA\d{2}\d{6}[a-zA-Z0-9]{19}$/, // Ukraine
+    VA: /^VA\d{2}\d{18}$/, // Vatican City State
+    VG: /^VG\d{2}[A-Z]{4}\d{16}$/, // Virgin Islands
+    XK: /^XK\d{2}\d{16}$/ // Kosovo
+};
+
+
+const IBAN_SUBSTITUTION_TABLE = {
+    A: '10',
+    B: '11',
+    C: '12',
+    D: '13',
+    E: '14',
+    F: '15',
+    G: '16',
+    H: '17',
+    I: '18',
+    J: '19',
+    K: '20',
+    L: '21',
+    M: '22',
+    N: '23',
+    O: '24',
+    P: '25',
+    Q: '26',
+    R: '27',
+    S: '28',
+    T: '29',
+    U: '30',
+    V: '31',
+    W: '32',
+    X: '33',
+    Y: '34',
+    Z: '35'
+};
+
+
+function modulo97(numStr) {
+    'use strict';
+
+    /*
+     * Input:
+     * numStr -- numeric string, very big positive integer
+     *
+     * Based on: https://en.wikipedia.org/wiki/International_Bank_Account_Number#Structure#Modulo_operation_on_IBAN
+     *
+     * Returns an integer, the remainder after division of the given number by 97.
+     */
+
+    const len = numStr.length;
+
+    let i = 9;
+    let n = parseInt(numStr.slice(0, i));
+
+    for (i; i < len; i += 7) {
+        n = parseInt(`${n % 97}${numStr.slice(i, i + 7)}`);
+    }
+
+    return n % 97;
+}
+
+
+/* exported isValid */
+function isValid(iban) {
+    'use strict';
+
+    iban = iban.replace(/\s/g, ''); // remove all whitespace characters
+
+    const countryCode = iban.slice(0, 2);
+
+    if (!IBAN_FORMATS.hasOwnProperty(countryCode)) return false;
+    if (!IBAN_FORMATS[countryCode].test(iban)) return false;
+
+    iban = iban.toUpperCase();
+    iban = `${iban.slice(4)}${iban.slice(0, 4)}`;
+    iban = iban.split('')
+        .map((char) => IBAN_SUBSTITUTION_TABLE[char] || char)
+        .join('');
+
+    return modulo97(iban) === 1;
 }
 
 
